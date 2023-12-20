bb95da7d5f3bcbdef2f407ae55aea31a98898eb0
max <max.iw15@yandex.ru>
2023-12-20 16:41:51 +0300

    finish

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import ShopItemFunc from "./components/Item.jsx";
+
+function App() {
+
+
+return (<>
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemFunc />
+        </div>
+      </div>
+    </>
+    )
+    }
+
+export default App
